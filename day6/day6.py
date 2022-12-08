114f62f040129953643b521f3bf06e3095c416a9
Ben Bicknell <benabicknell@gmail.com>
2022-12-08 11:18:36 -0500

    day6 done

--- day6/day6.py
+++ day6/day6.py
@@ -5,9 +5,9 @@ input_data.close()
 
 def func(seq, n):
     signal = ["".join(char) for char in zip(*[seq[n:] for n in range(n)])]
-    for count, i in enumerate(signal, 4):
-        if len(set(i)) == 4:
+    for count, i in enumerate(signal, n):
+        if len(set(i)) == n:
             return count, i
 
 
-print(func(line, 4))
+print(func(line, 14))
